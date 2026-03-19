542e0ee4b97769db0878ae1c6db27f5e53060750
spnaumowicz <stefan_naumowicz837@sunyempire.edu>
2026-03-19 12:47:32 -0400

    Update deploy-prod.sh

--- ops/deploy-prod.sh
+++ ops/deploy-prod.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-cd /home/stefan/pm-prod/ADK-Digital-Site
+cd /var/home/local-admin/pm/bscsd-pm
 
 echo "Pulling latest code..."
 git pull
 
 echo "Rebuilding and starting production stack..."
-docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
+docker compose -f docker-compose.prod4.yml up -d --build
 
 echo "Current container status:"
 docker ps

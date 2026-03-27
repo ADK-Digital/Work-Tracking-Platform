e1e2be6179412669613eb4bb940232236771bb0b
spnaumowicz <stefan_naumowicz837@sunyempire.edu>
2026-03-27 13:57:00 -0400

    Update deploy-prod.sh

--- ops/deploy-prod.sh
+++ ops/deploy-prod.sh
@@ -7,7 +7,7 @@ echo "Pulling latest code..."
 git pull
 
 echo "Rebuilding and starting production stack..."
-docker compose -f docker-compose.prod.yml up -d --build
+sudo podman-compose -f docker-compose.prod.yml up -d --build
 
 echo "Current container status:"
-docker ps
+sudo podman ps
